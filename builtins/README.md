84c8d2538952c5e1e525924890a6de2a3cdd70c1
Alexander Tokorv <71885787+tokorv@users.noreply.github.com>
2022-02-03 16:08:26 +0000

    Finished built-in functions

--- builtins/README.md
+++ builtins/README.md
@@ -5,17 +5,19 @@ These are things like:
 **Datatypes**: `void, bool, int, float, string`  
   
 **Functions**:
-- `Print()`
-- `Write()`
-- `Input()`
-- `InputKey()`
-- `Clear()`
-- `SetCursor()`
-- `GetSizeX()`
-- `GetSizeY()`
-- `SetSize()`
-- `SetCursorVisible()`
-- `GetCursorVisible()`
-- `Random()`
-- `Sleep()`
-- `Version()`
+| Function  | Done |
+| ------------- | ------------- |
+| `Print()`  | :white_check_mark:  |
+| `Write()`  | :white_check_mark:  |
+| `Input()`  | :white_check_mark:  |
+| `InputKey()`  | :white_check_mark:  |
+| `Clear()`  | :white_check_mark:  |
+| `SetCursor()`  | :white_check_mark:  |
+| `GetSizeX()`  | :white_check_mark:  |
+| `GetSizeY()`  | :white_check_mark:  |
+| `SetSize()`  | :white_check_mark:  |
+| `SetCursorVisible()`  | :white_check_mark:  |
+| `GetCursorVisible()`  | :white_check_mark:  |
+| `Random()` | :white_check_mark:  |
+| `Sleep()`  | :white_check_mark:  |
+| `Version()` | :white_check_mark:  |
--- builtins/builtin-functions.go
+++ builtins/builtin-functions.go
@@ -30,4 +30,93 @@ var (
 		String,
 		nodes.FunctionDeclarationMember{},
 	)
+
+	InputKey = symbols.CreateFunctionSymbol(
+		"InputKey",
+		[]symbols.ParameterSymbol{},
+		String,
+		nodes.FunctionDeclarationMember{},
+	)
+
+	Clear = symbols.CreateFunctionSymbol(
+		"Clear",
+		[]symbols.ParameterSymbol{},
+		Void,
+		nodes.FunctionDeclarationMember{},
+	)
+
+	SetCursor = symbols.CreateFunctionSymbol(
+		"SetCursor",
+		[]symbols.ParameterSymbol{
+			symbols.CreateParameterSymbol("x", 0, Int),
+			symbols.CreateParameterSymbol("y", 0, Int),
+		},
+		Void,
+		nodes.FunctionDeclarationMember{},
+	)
+
+	GetSizeX = symbols.CreateFunctionSymbol(
+		"GetSizeX",
+		[]symbols.ParameterSymbol{},
+		Int,
+		nodes.FunctionDeclarationMember{},
+	)
+
+	GetSizeY = symbols.CreateFunctionSymbol(
+		"GetSizeY",
+		[]symbols.ParameterSymbol{},
+		Int,
+		nodes.FunctionDeclarationMember{},
+	)
+
+	SetSize = symbols.CreateFunctionSymbol(
+		"GetSize",
+		[]symbols.ParameterSymbol{
+			symbols.CreateParameterSymbol("width", 0, Int),
+			symbols.CreateParameterSymbol("height", 0, Int),
+		},
+		Void,
+		nodes.FunctionDeclarationMember{},
+	)
+
+	SetCursorVisible = symbols.CreateFunctionSymbol(
+		"SetCursorVisible",
+		[]symbols.ParameterSymbol{
+			symbols.CreateParameterSymbol("state", 0, Bool),
+		},
+		Void,
+		nodes.FunctionDeclarationMember{},
+	)
+
+	GetCursorVisible = symbols.CreateFunctionSymbol(
+		"GetCursorVisible",
+		[]symbols.ParameterSymbol{},
+		Bool,
+		nodes.FunctionDeclarationMember{},
+	)	
+
+	Random = symbols.CreateFunctionSymbol(
+		"Random",
+		[]symbols.ParameterSymbol{
+			symbols.CreateParameterSymbol("maxNum", 0, Int),
+		},
+		Int,
+		nodes.FunctionDeclarationMember{},
+	)
+
+	Sleep = symbols.CreateFunctionSymbol(
+		"Sleep",
+		[]symbols.ParameterSymbol{
+			symbols.CreateParameterSymbol("milliseconds", 0, Int),
+		},
+		Void,
+		nodes.FunctionDeclarationMember{},
+	)
+
+	Version = symbols.CreateFunctionSymbol(
+		"Version",
+		[]symbols.ParameterSymbol{},
+		String,
+		nodes.FunctionDeclarationMember{},
+	)
 )
