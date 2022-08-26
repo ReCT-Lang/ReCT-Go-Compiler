4392c43e18381c05593c42b7366251ea2a7d0416
Mauro Baladés <mauro.balades@tutanota.com>
2022-08-26 15:50:50 +0200

    added CPP linking

--- run_lin.sh
+++ run_lin.sh
@@ -3,6 +3,6 @@
 opt ./out.ll > ./out.bc
 opt ./packages/sys.ll > ./packages/sys.bc
 llvm-link ./out.bc ./systemlib/systemlib_lin.bc ./packages/sys.bc > ./program.bc
-clang -lm -pthread -rdynamic ./program.bc -o ./program
+clang -lc++ -lm -pthread -rdynamic ./program.bc -o ./program
 
 ./program
